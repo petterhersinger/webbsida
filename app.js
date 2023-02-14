a13ddd718158315cb095d9521a4a9186c86cc5f6
Petter Hersinger <petterhersinger@gmail.com>
2023-02-14 13:00:04 +0100

    valid

--- app.js
+++ app.js
@@ -6,9 +6,24 @@ $(document).ready(function () {
         email: {
             required: true,
             email: true
+    },
+    messages: {
+      name: "Please enter your name: <br>",
+      message: "You need to send a message to Ghost<br>",
+      email: {
+        required: "Please enter your email adress<br>",
+        email: "Please enter a valid email adress<br>"
+
+        }
+      }
     }
-  }
-})});
+  });
+});
+$(document).ready(function () {
+  $("#print-link").click(function() {
+    window.print();
+  });
+});
 
 function button1listener(){
   document.getElementById("image").src="resources/rpk.jpg";
